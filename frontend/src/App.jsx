364165c909e3bc30c0c1555b025d14f85a96f2d3
Zach Harvey <Zach@zach-harveys-macbook-pro-2.local>
2019-10-02 18:05:14 -0600

    WIP POST

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,20 @@ import React, { Component } from "react";
 import { hot } from "react-hot-loader";
 import axios from 'axios'
 import "./App.css";
+import InputMask from 'react-input-mask';
 
 class App extends Component {
 
-    state = {
-        expenses: []
+    constructor(props) {
+        super(props)
+        this.state = {
+            expenses: [],
+            title: '',
+            description: '',
+            category: ["Food", "Entertainment", "Utilities"],
+            amount: '',
+            mask: ''
+        }
     }
 
     componentDidMount() {
@@ -17,19 +26,36 @@ class App extends Component {
             })
     }
 
+
+    PostExpense = (e) => {
+        const { title, description, category, amount } = this.state
+        axios.post('http://localhost:8080/expenses', {title, description, category, amount})
+
+            .then(res  => {
+                const expenses = res.data;
+                this.setState({expenses})
+            })
+    }
+
+    onChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
     render() {
+
+        const { title, description, category, amount } = this.state
+
         return (
-            <div>
-                <span>Title<input type="text"/></span>
-                <span>Description<input type="text"/></span>
-                <span>Category
-                    <select>
-                        <option value="Food">Food</option>
-                        <option value="Entertainment">Entertainment</option>
-                        <option value="Utilities">Utilities</option>
+            <form>
+                <input placeholder='TITLE' type="text" name="title" value={title} onChange={this.onChange}/>
+                <input placeholder='DESCRIPTION' type="text" name="description" value={description} onChange={this.onChange}/>
+                    <select placeholder='CATEGORY'>
+                        <option name="Food" value={category[0]}>Food</option>
+                        <option name="Entertainment" value={category[1]}>Entertainment</option>
+                        <option name="Utilities" value={category[2]}>Utilities</option>
                     </select>
-                </span>
-                <span>Amount $<input type="text"/></span>
+                <InputMask mask='99.99' placeholder='AMOUNT' type="text" name="amount" value={amount} onChange={this.onChange}/>
+                <button type="submit" onClick={this.PostExpense}>Submit</button>
                 <div>Current Expenses</div>
                 <div>
                     {this.items = this.state.expenses.map((item, key) =>
@@ -41,7 +67,7 @@ class App extends Component {
                         </li>
                     )}
                 </div>
-            </div>
+            </form>
         );
     }
 }
