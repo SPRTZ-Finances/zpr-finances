751208c963e7de72864d8526bd2db7b08e7e0e5a
Phil <pbastien003@gmail.com>
2019-10-05 13:12:19 -0400

    Add 3 new components (Header, Footer and ListExpenses)

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Footer extends Component {
+    render() {
+        return(
+            <footer>
+                <br />
+                <span>All Right Reserved 2019 ZPR Finances</span>
+            </footer>
+        )
+    }
+}
+
+export default Footer
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Header extends Component {
+    render() {
+        return(
+            <header>
+                <div>ZPR Finances <span>💰</span></div>
+                <br />
+            </header>
+        )
+    }
+}
+
+export default Header
